4377ba3e16f3af169e5f760f7bd7de9a496c5e10
Aparna-B-M <aparnapalasseril123@gmail.com>
2025-07-19 22:52:54 +0530

    bubble game implementation with toggle

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AlphabetSoundBubblesComponent } from './alphabet-sound-bubbles/alphabet-sound-bubbles.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone:true,
+  imports: [RouterOutlet,AlphabetSoundBubblesComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
